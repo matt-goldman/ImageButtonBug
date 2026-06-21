79de52813726fe824b2b1db9e0f4d113281744aa
Matt Goldman <matt.goldman@oultook.com>
2026-06-21 12:00:26 +1000

    Iniital commit

---
+++ ImageButtonBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageButtonBug;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ImageButtonBug/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ImageButtonBug;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Phosphor.ttf", "Phosphor");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
